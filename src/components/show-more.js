fa9a4bfeba170caf2fcdcb3e849129a1462e4c03
Yulia Elagina <julia-elagina@yandex.ru>
2020-04-19 17:05:50 +0300

    Создает метод для навешивания обработчика на кнопку ShowMore

--- src/components/show-more.js
+++ src/components/show-more.js
@@ -4,4 +4,8 @@ export default class ShowMore extends AbstractComponent {
   getTemplate() {
     return `<button class="films-list__show-more">Show more</button>`;
   }
+
+  setClickHandler(handler) {
+    this.getElement().addEventListener(`click`, handler);
+  }
 }
