f34fb36738c81e3e481f083e15a6d6b7a130dcbc
Yulia Elagina <julia-elagina@yandex.ru>
2020-05-03 12:57:04 +0300

    Добавляет модель для комментариев

---
+++ src/models/comments.js
@@ -0,0 +1,29 @@
+export default class Comments {
+  constructor() {
+    this._comments = [];
+    this._dataChangeHandlers = [];
+  }
+
+  getAllComments() {
+    return this._comments;
+  }
+
+  getCommentsByIds(ids) {
+    return ids.map((id) => {
+      return this._comments.find((item) => item.id === id);
+    });
+  }
+
+  setComments(comments) {
+    this._comments = comments;
+    this._callHandlers(this._dataChangeHandlers);
+  }
+
+  setDataChangeHandler(handler) {
+    this._dataChangeHandlers.push(handler);
+  }
+
+  _callHandlers(handlers) {
+    handlers.forEach((handler) => handler());
+  }
+}
