c3de231af99379df3942312f2bb1986f66e342ef
Yulia Elagina <julia-elagina@yandex.ru>
2020-05-17 01:54:34 +0300

    Выводит сообщение Loading во время загрузки данных с сервера и сообщение-заглушку при неудаче

--- src/components/cards-list.js
+++ src/components/cards-list.js
@@ -1,10 +1,11 @@
 import AbstractComponent from "./abstract-component";
+import {HIDDEN_CLASS} from "../const.js";
 
 const getCardsListTemplate = () => {
   return (
     `<section class="films">
       <section class="films-list">
-        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+        <h2 class="films-list__title visually-hidden">Loading...</h2>
         <div class="films-list__container">
         </div>
       </section>
@@ -15,5 +16,10 @@ const getCardsListTemplate = () => {
 export default class CardsList extends AbstractComponent {
   getTemplate() {
     return getCardsListTemplate();
+  }
+
+  showLoadingMessage() {
+    const messageElement = this.getElement().querySelector(`.films-list__title`);
+    messageElement.classList.remove(HIDDEN_CLASS);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 import API from "./api.js";
+import CardsListComponent from "./components/cards-list.js";
 import FilterController, {getFilmsByFilter} from "./controllers/filter.js";
 import FooterStatComponent from "./components/footer-stat.js";
 import MoviesModel from "./models/movies.js";
+import NoFilmsComponent from "./components/no-films.js";
 import PageController from "./controllers/page-controller.js";
 import SiteNavComponent from "./components/site-nav.js";
+import SortComponent from "./components/sort.js";
 import StatisticsComponent from "./components/statistics.js";
 import UserProfileComponent from "./components/user-profile.js";
-import {RenderPosition, render} from "./utils/render.js";
+import {RenderPosition, render, remove} from "./utils/render.js";
 import {NavType, FilterType} from "./const.js";
 
 const AUTHORIZATION = `Basic f8Lid33jXHpo4/?`;
 const END_POINT = `https://11.ecmascript.pages.academy/cinemaddict`;
 
-const bodyElement = document.querySelector(`body`);
+const bodyElement = document.body;
 const pageHeaderElement = bodyElement.querySelector(`.header`);
 const pageMainElement = bodyElement.querySelector(`.main`);
 const footerStatisticsElement = bodyElement.querySelector(`.footer__statistics`);
@@ -22,9 +25,14 @@ const api = new API(AUTHORIZATION, END_POINT);
 const moviesModel = new MoviesModel();
 
 const siteNavComponent = new SiteNavComponent();
+const sortComponent = new SortComponent();
+
+const cardsListComponent = new CardsListComponent();
+
+
 const statisticsComponent = new StatisticsComponent(moviesModel);
 const filterController = new FilterController(siteNavComponent.getElement(), moviesModel);
-const pageController = new PageController(pageMainElement, bodyElement, moviesModel, api);
+const pageController = new PageController(pageMainElement, sortComponent, moviesModel, api);
 
 siteNavComponent.setOnChangeHandler((navItem) => {
   if (navItem === NavType.STATS) {
@@ -38,16 +46,26 @@ siteNavComponent.setOnChangeHandler((navItem) => {
 });
 
 render(pageMainElement, siteNavComponent);
+filterController.render();
+render(pageMainElement, sortComponent);
+
+render(pageMainElement, cardsListComponent);
+cardsListComponent.showLoadingMessage();
 // Экран со статистикой
 render(pageMainElement, statisticsComponent);
 statisticsComponent.hide();
-
+debugger;
 api.getMovies()
 .then((movies) => {
   moviesModel.setMovies(movies);
   const userLevel = getFilmsByFilter(FilterType.HISTORY, movies).length;
   render(pageHeaderElement, new UserProfileComponent(userLevel));
-  filterController.render();
+  remove(sortComponent);
+  remove(cardsListComponent);
   pageController.render();
   render(footerStatisticsElement, new FooterStatComponent(movies.length), RenderPosition.AFTERBEGIN);
+})
+.catch(() => {
+  remove(cardsListComponent);
+  render(pageMainElement, new NoFilmsComponent());
 });
